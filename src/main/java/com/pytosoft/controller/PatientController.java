40c5b587fec0e984e7a11d1170436f1fe2706046
chetan <csarthore617@gmail.com>
2022-12-26 15:19:23 +0530

    GiqliParamValueConroller And PatientController CrudOperations Updeted

--- src/main/java/com/pytosoft/controller/PatientController.java
+++ src/main/java/com/pytosoft/controller/PatientController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.pytosoft.constants.DetailMode;
+import com.pytosoft.model.GiqliParamValue;
 import com.pytosoft.model.Patient;
 import com.pytosoft.service.PatientService;
 
@@ -27,14 +29,11 @@ public class PatientController// extends BaseController
 	@Autowired
 	private PatientService patientService;
 
-	@RequestMapping(value = "/patientassignments", method = RequestMethod.POST)
-	@ResponseBody
-	public ResponseEntity<Patient> assignPatientToDoctor(@RequestBody Patient patient, HttpServletRequest request)
-	{
-		patientService.assignPatientToDoctor(patient);
-		return new ResponseEntity<Patient>(HttpStatus.CREATED);
+	@PostMapping("/patientassignments")
+	public ResponseEntity<Patient> addPatient(@RequestBody Patient patient) {
+		Patient saved = patientService.savePatient(patient);
+		return new ResponseEntity<Patient>(saved,HttpStatus.OK);
 	}
-
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	@ResponseBody
@@ -44,20 +43,18 @@ public class PatientController// extends BaseController
 		Patient patient = patientService.getPatientById(id, detailMode, accessCode);
 		return new ResponseEntity<Patient>(patient, HttpStatus.OK);
 	}
+//
+//	@PutMapping("/patient/{id}")
+//	public Patient updatepatient(@RequestBody Patient patient,@PathVariable("id") Long id) {
+//		return patientService.updatepatient(patient, id);
+//		
+//	}
 
-	@PutMapping("/patient/{id}")
-	public Patient updatepatient(@RequestBody Patient patient,@PathVariable("id") Long id) {
-		
-		
-		return patientService.updatepatient(patient, id);
-		
-	}
 
 	@DeleteMapping("/{id}")
-	public String delete(@PathVariable Long id) {
-		 
+	public ResponseEntity<String> delete(@PathVariable Long id) {
 		patientService.delete(id);
-		return " Deleted Patient with id" +id;
+	return new ResponseEntity<String>("deleted"+id,HttpStatus.OK);
 	}
 
 }
