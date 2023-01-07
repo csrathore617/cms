a4e0ff2f5844a5514857441cd10d3b72814a09cf
vikram <vikramshekhawat451@gmail.com>
2023-01-07 12:06:28 +0530

    new feature added in clinic

--- src/main/java/com/pytosoft/service/DoctorClinicAssignmentService.java
+++ src/main/java/com/pytosoft/service/DoctorClinicAssignmentService.java
@@ -17,76 +17,63 @@ public class DoctorClinicAssignmentService {
 
 	@Autowired
 	private DoctorClinicAssignmentRepository doctorClinicAssignmentRepository;
-	
+
 	@Autowired
 	private DoctorRepository doctorRepository;
-	
+
 	@Autowired
 	private ClinicRepository clinicRepository;
-	
-	public List<DoctorClinicAssignment> findByDoctorAndClinic(Long docId,Integer clinicId)
-	{
+
+	public List<DoctorClinicAssignment> findByDoctorAndClinic(Long docId, Integer clinicId) {
 		Doctor doctor = doctorRepository.findById(docId).get();
-        Clinic clinic = clinicRepository.findById(clinicId).get();
-        
+		Clinic clinic = clinicRepository.findById(clinicId).get();
+
 		return doctorClinicAssignmentRepository.findByDoctorAndClinic(doctor, clinic);
-		
+
 	}
-	
-	public List<DoctorClinicAssignment> findAllByDoctor(Long docId)
-	{
+
+	public List<DoctorClinicAssignment> findAllByDoctor(Long docId) {
 		Doctor doctor = doctorRepository.findById(docId).get();
 		return doctorClinicAssignmentRepository.findByDoctor(doctor);
-		
+
 	}
-	public List<DoctorClinicAssignment> findByClinic(Integer clinicId)
-	{
+
+	public List<DoctorClinicAssignment> findByClinic(Integer clinicId) {
 		Clinic clinic = clinicRepository.findById(clinicId).get();
 		return doctorClinicAssignmentRepository.findByClinic(clinic);
-		
+
 	}
-	
-	public List<DoctorClinicAssignment> findAll()
-	{
+
+	public List<DoctorClinicAssignment> findAll() {
 		return doctorClinicAssignmentRepository.findAll();
-		
+
 	}
-	
-	public DoctorClinicAssignment findById(Long id)
-	{
+
+	public DoctorClinicAssignment findById(Long id) {
 		return doctorClinicAssignmentRepository.findById(id).get();
-		
+
 	}
-	
-	public void save(List<DoctorClinicAssignment> listOfDoctorClinicAssignment,Long docId,Integer clinicId)
-	{
-		 Doctor doctor = doctorRepository.findById(docId).get();
-		   Clinic clinic = clinicRepository.findById(clinicId).get();
-		   
-	  for (DoctorClinicAssignment doctorClinicAssignment : listOfDoctorClinicAssignment) {
-		
-		 
-			
+
+	public void save(List<DoctorClinicAssignment> listOfDoctorClinicAssignment, Long docId, Integer clinicId) {
+		Doctor doctor = doctorRepository.findById(docId).get();
+		Clinic clinic = clinicRepository.findById(clinicId).get();
+
+		for (DoctorClinicAssignment doctorClinicAssignment : listOfDoctorClinicAssignment) {
+
 			doctorClinicAssignment.setDoctor(doctor);
 			doctorClinicAssignment.setClinic(clinic);
-			
-			 doctorClinicAssignmentRepository.save(doctorClinicAssignment);
+
+			doctorClinicAssignmentRepository.save(doctorClinicAssignment);
+		}
+
 	}
-		
-	}
-	
-	public void deleteById(Long id)
-	{
+
+	public void deleteById(Long id) {
 		DoctorClinicAssignment doctorClinicAssignment = doctorClinicAssignmentRepository.findById(id).get();
 		doctorClinicAssignment.setActive(false);
-		
+
 		doctorClinicAssignmentRepository.save(doctorClinicAssignment);
-		
+
 	}
-	
-//	public void update(Long id,DoctorClinicAssignment doctorClinicAssignment)
-//	{
-//		 
-//		
-//	}
+
 }
